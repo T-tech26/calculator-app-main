cccc1fa2c5a5082127ab7408c0cb529d75bfd4cc
T-tech26 <94144505+T-tech26@users.noreply.github.com>
2023-03-16 04:30:53 +0530

    Code comments and links update

--- docs/README.md
+++ docs/README.md
@@ -26,8 +26,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Add solution URL here](https://t-tech26.github.io/calculator-app-main/)
+- Live Site URL: [Add live site URL here](https://t-tech26.github.io/calculator-app-main/)
 
 ### Built with
 
--- docs/script.js
+++ docs/script.js
@@ -7,6 +7,9 @@ const screen = document.querySelector('.screen');
 const values = [];
 let theme;
 
+/* The click event on the radio boxes will store the theme to the localstorage, this is because if the user revisits the app we want to set the theme to the last theme the user selected before leaving the      page, so not to make the visitor reselect it again.
+  When the theme is stored in the localstorage, the body will be applying the theme stored directly from the localstorage, so that the last theme is applied, then removing other themes from the body tag.
+*/
 themeOne.addEventListener('click', () => {
   localStorage.setItem('Theme', 'theme-first');
   theme = localStorage.getItem('Theme');
@@ -40,6 +43,9 @@ themeThree.addEventListener('click', () => {
   themeTwo.checked = false;
 })
 
+/* This variable function is call from the load event listener from the bottom of the code, to apply the themes based on the user system settings, if dark or light mode.
+  First it checks if the user has visited the page before by checking the users localstorage to see if the user has stored any theme of his choice, if not check if the system is set to dark or light mode, then if dark mode set to the first theme, if light mode set to the second theme, else if the user has visited the app before set the theme to the last theme the user selected which is stored in the localstorage
+*/
 const userSelectedTheme = () => {
   if(localStorage.getItem('Theme') === null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     body.classList.add('theme-first');
@@ -76,6 +82,11 @@ const userSelectedTheme = () => {
   }
 }
 
+/* The add to screen variable function is used to display the content of the keys pressed then display them to the calculator screen
+  As well check if the key pressed are Delete, Reset and equal to keys to add their own functions to them
+  The inputs gotten from the keys a push to an array, so as to make use of some of the array methods, like pop to delete from when the delete key is pressed, and splice to empty the array when the reset key is pressed, then the content of the keys are push to the array as the default for the switch statement.
+  This can be done with the if else statement, but just thought to make use of switch. 
+*/
 const addToScreen = (value) => {
   let answer;
 
@@ -100,6 +111,9 @@ const addToScreen = (value) => {
     value == '=' ? screen.textContent = answer : screen.textContent = values.join('');
 }
 
+/* This is basically a for each loop to get all the keys pressed regardless of which keys are been pressed
+  Then a add to screen function is called to display the content of the key pressed to the calculator screen
+*/
 btns.forEach((value) => {
   value.addEventListener('click', () => {
     addToScreen(value.textContent);
